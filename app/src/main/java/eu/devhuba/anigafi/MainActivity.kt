c997e2d9d397913b206fbce8acd6aec4c50b0c5e
aleksandrs <devhuba@gmail.com>
2023-05-05 19:58:09 +0300

    try swipe on screens

--- app/src/main/java/eu/devhuba/anigafi/MainActivity.kt
+++ app/src/main/java/eu/devhuba/anigafi/MainActivity.kt
@@ -3,6 +3,7 @@ package eu.devhuba.anigafi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -11,11 +12,23 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.rememberPagerState
 import eu.devhuba.anigafi.ui.theme.AniGaFiTheme
 import eu.devhuba.anigafi.view.AnimeScreen
 import eu.devhuba.anigafi.view.FilmsScreen
 
 sealed class Destination(val route: String) {
+	
+	//Count our screens
+	companion object {
+		var countObjects = 0
+	}
+	
+	init {
+		countObjects++
+	}
+	
 	object Anime : Destination("anime")
 	object Films : Destination("films")
 	object Games : Destination("games")
@@ -44,19 +57,35 @@ class MainActivity : ComponentActivity() {
 	}
 }
 
+@OptIn(ExperimentalPagerApi::class)
 @ExperimentalMaterialApi
 @Composable
 fun AppScaffold(navController: NavHostController) {
 	
 	val scaffoldState = rememberScaffoldState()
 	val swappableState = rememberSwipeableState(0)
+	val pagerState = rememberPagerState(initialPage = 0)
+	val countOfDestinations = Destination.countObjects
+	
 	
 	Scaffold(
-		scaffoldState = scaffoldState,
 		topBar = { },
-		bottomBar = { }
+		bottomBar = { },
+		scaffoldState = scaffoldState
 	) { paddingValues ->
-		NavHost(navController = navController, startDestination = Destination.Anime.route) {
+		NavHost(
+			navController = navController, startDestination = Destination.Anime.route,
+			modifier = Modifier.fillMaxSize()
+					.swipeable(
+						state = swappableState,
+						anchors = mapOf(
+							-1f to 0,
+							0f to 1,
+							1f to 2
+						),
+						orientation = Orientation.Horizontal
+					)
+		) {
 			composable(Destination.Anime.route) {
 				AnimeScreen(navController, paddingValues)
 			}
