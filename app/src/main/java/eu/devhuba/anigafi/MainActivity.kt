8a9ee30c55f08f6686033b95baef2c4a81d6c594
aleksandrs <devhuba@gmail.com>
2023-05-05 14:17:28 +0300

    screens setup

--- app/src/main/java/eu/devhuba/anigafi/MainActivity.kt
+++ app/src/main/java/eu/devhuba/anigafi/MainActivity.kt
@@ -1,175 +1,68 @@
 package eu.devhuba.anigafi
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import eu.devhuba.anigafi.ui.theme.AniGaFiTheme
+import eu.devhuba.anigafi.view.AnimeScreen
+import eu.devhuba.anigafi.view.FilmsScreen
+
+sealed class Destination(val route: String) {
+	object Anime : Destination("anime")
+	object Films : Destination("films")
+	object Games : Destination("games")
+
+//    object CharacterDetail : Destination("character/{characterId}") {
+//        fun createRoute(characterId: Int?) = "character/$characterId"
+//    }
+}
 
 class MainActivity : ComponentActivity() {
-    @ExperimentalMaterialApi
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            AniGaFiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    val navController = rememberNavController()
-                    MyApp(navController = navController)
-                }
-            }
-        }
-    }
+	@ExperimentalMaterialApi
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			AniGaFiTheme {
+				// A surface container using the 'background' color from the theme
+				Surface(
+					modifier = Modifier.fillMaxSize(),
+					color = MaterialTheme.colors.background
+				) {
+					val navController = rememberNavController()
+					AppScaffold(navController = navController)
+				}
+			}
+		}
+	}
 }
 
 @ExperimentalMaterialApi
 @Composable
-fun MyApp(navController: NavController) {
-
-    val swappableState = rememberSwipeableState(0)
-
-    Scaffold(
-        topBar = { },
-        bottomBar = {
-            Box(
-                modifier = Modifier.fillMaxWidth()
-                    .height(25.dp).border( 4.dp, color = Color.Red ),
-                contentAlignment = Alignment.Center,
-            ) {
-                Text(text = "this is bottom bar", 
-                    fontSize = 30.sp)
-            }
-        }
-    ) { paddingValues ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .swipeable(
-                    state = swappableState,
-                    anchors = mapOf(
-                        0f to 0,
-                        1f to 1
-                    ),
-                    thresholds = { _, _ -> FractionalThreshold(0.5f) },
-                    orientation = Orientation.Horizontal,
-                    reverseDirection = true,
-                )
-                .background(Color.White)
-        ) {
-            val currentPage = swappableState.currentValue
-            val targetPage = swappableState.targetValue
-
-            when (currentPage) {
-                0 -> {
-                    Screen1()
-                }
-                1 -> {
-                    Screen2()
-                }
-                2 -> {
-                    Screen3()
-                }
-            }
-
-            when (targetPage) {
-                0 -> {
-                    SwipeToScreen1()
-                }
-                1 -> {
-                    SwipeToScreen2()
-                }
-                2 -> {
-                    SwipeToScreen3()
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Screen1() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Screen 1", fontSize = 24.sp)
-    }
-}
-
-@Composable
-fun Screen2() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Screen 2", fontSize = 24.sp)
-    }
-}
-
-@Composable
-fun Screen3() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Screen 3", fontSize = 24.sp)
-    }
-}
-
-@Composable
-fun SwipeToScreen1() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Green),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Swipe right to go to Screen 1", color = Color.White, fontSize = 24.sp)
-    }
-}
-
-@Composable
-fun SwipeToScreen2() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Blue),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Swipe left to go to Screen 2", color = Color.White, fontSize = 24.sp)
-    }
-}
-
-@Composable
-fun SwipeToScreen3() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Red),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Swipe left to go to Screen 3", color = Color.White, fontSize = 24.sp)
-    }
+fun AppScaffold(navController: NavHostController) {
+	
+	val scaffoldState = rememberScaffoldState()
+	val swappableState = rememberSwipeableState(0)
+	
+	Scaffold(
+		scaffoldState = scaffoldState,
+		topBar = { },
+		bottomBar = { }
+	) { paddingValues ->
+		NavHost(navController = navController, startDestination = Destination.Anime.route) {
+			composable(Destination.Anime.route) {
+				AnimeScreen(navController, paddingValues)
+			}
+			composable(Destination.Films.route) {
+				FilmsScreen(navController, paddingValues)
+			}
+		}
+	}
 }
---
+++ app/src/main/java/eu/devhuba/anigafi/view/AnimeScreen.kt
@@ -0,0 +1,47 @@
+package eu.devhuba.anigafi.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+@Composable fun AnimeScreen(navController: NavHostController, paddingValues: PaddingValues) {
+	
+	Column(
+		modifier = Modifier.fillMaxSize()
+				.padding(bottom = paddingValues.calculateBottomPadding())
+				.background(Color.Black),
+		horizontalAlignment = Alignment.CenterHorizontally,
+	) {
+		Column(
+			modifier = Modifier.fillMaxSize(),
+			horizontalAlignment = Alignment.CenterHorizontally,
+			verticalArrangement = Arrangement.Center
+		) {
+			ShowAnimeList(navController)
+		}
+	}
+	
+}
+
+@Composable fun ShowAnimeList(navController: NavHostController) {
+	val mockListOfAnime = listOf(
+		"Demon Slayer", "Dorohedoro", "Naruto", "Baruto", "Shmaruto", "One Piece",
+		"HunterVSHunter"
+	)
+	
+	LazyColumn {
+		items(mockListOfAnime) { anime ->
+			Text(text = anime, fontSize = 30.sp, color = Color.Yellow)
+		}
+	}
+}
+
+
