2f17bc335c04f3e668b1f37fa35a26d5869c2b7d
Aleks_S <devhuba@gmail.com>
2023-05-16 23:21:42 +0300

    bit more api with error

--- app/src/main/java/eu/devhuba/anigafi/model/api/ApiService.kt
+++ app/src/main/java/eu/devhuba/anigafi/model/api/ApiService.kt
@@ -1,15 +1,25 @@
 package eu.devhuba.anigafi.model.api
 
-import android.os.Build
 import eu.devhuba.anigafi.BuildConfig
+import okhttp3.HttpUrl
+import okhttp3.Request
 import retrofit2.Retrofit
 
 object ApiService {
-	private const val BASE_URL = "https://shikimori.me/api/"
-	
-	private fun getRetrofit(): Retrofit {
-		val authToken = BuildConfig.ANIME_AUTH_TOKEN
-		
+    private const val BASE_URL = "https://shikimori.me/api/"
 
-	}
+    private fun getRetrofit(): Retrofit {
+        val clientId = BuildConfig.ANIME_CLIENT_ID
+        val secret = BuildConfig.ANIME_SECRET
+        val code = BuildConfig.ANIME_AUTH_CODE
+        val redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+
+        val clientInterceptor = okhttp3.Interceptor { chain ->
+            var request: Request = chain.request()
+            val url: HttpUrl = request.url.newBuilder().add
+            
+        }
+
+
+    }
 }
