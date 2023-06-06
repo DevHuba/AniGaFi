62bee7ea181bc50c4a7704724250374636868b70
Aleks_S <devhuba@gmail.com>
2023-06-06 14:47:26 +0300

    try  by ru source

---
+++ app/src/main/java/eu/devhuba/anigafi/view/auth/LogScreen.kt
@@ -0,0 +1,59 @@
+package eu.devhuba.anigafi.view.auth
+
+import android.net.Uri
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavHostController
+import eu.devhuba.anigafi.R
+import eu.devhuba.anigafi.model.ApiConstants
+import eu.devhuba.anigafi.ui.theme.DarkBlack
+import eu.devhuba.anigafi.viewmodel.MainViewModel
+
+@Composable
+fun LogScreen(
+	navController: NavHostController,
+	paddingValues: PaddingValues,
+	mvm: MainViewModel,
+) {
+	val context = LocalContext.current
+	
+	Column(
+		modifier = Modifier
+				.fillMaxSize()
+				.background(DarkBlack),
+	) {
+		
+		Image(
+			painter = painterResource(id = R.drawable.logo),
+			modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
+			contentDescription = null
+		)
+		
+		Button(
+			modifier = Modifier.align(Alignment.CenterHorizontally),
+			onClick = {
+				
+				mvm.login()
+				
+				val authUrl = ApiConstants.ANIME_AUTH_URI
+				val customTabsIntent = CustomTabsIntent.Builder()
+						.build()
+				customTabsIntent.launchUrl(context, Uri.parse(authUrl))
+				
+			}) {
+			Text(text = "Login for Anime data")
+		}
+	}
+	
+}
