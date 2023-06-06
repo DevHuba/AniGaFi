d5987cca7308fdb7d992c40c8b54d6de00398a00
Aleks_S <devhuba@gmail.com>
2023-06-06 14:54:51 +0300

    timber + dependency

--- app/src/main/java/eu/devhuba/anigafi/AniGaFiApplication.kt
+++ app/src/main/java/eu/devhuba/anigafi/AniGaFiApplication.kt
@@ -2,7 +2,37 @@ package eu.devhuba.anigafi
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class AniGaFiApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(object : Timber.DebugTree() {
+            override fun log(
+                priority: Int,
+                tag: String?,
+                message: String,
+                t: Throwable?
+            ) {
+                super.log(priority, "global_tag_$tag", message, t)
+            }
+
+            //Adds method name into log 
+            override fun createStackElementTag(element: StackTraceElement): String {
+                return String.format(
+                    "%s:%s",
+                    element.methodName,
+                    super.createStackElementTag(element)
+                )
+            }
+
+        })
+
+        //Usage
+        Timber.d("App created ! ")
+
+    }
 }
